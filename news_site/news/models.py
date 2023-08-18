3ce75f80fa8da678be8c9a9a01e2fd13795ba64a
git-student2021 <student.andrey.2022@gmail.com>
2023-08-18 15:31:48 +0300

    add model class News in models/news

--- news_site/news/models.py
+++ news_site/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)   # все поля обязательны к заполнению если blank=True то запишется пустая строка
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d') # upload_to='photos/%Y/%m/%d' django cоздаст папку и будет хранить изображния
+    is_publishished = models.BooleanField(default=True)
