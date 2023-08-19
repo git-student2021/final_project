78a47c3af2b91967407d1daa37cb82373ce7a881
git-student2021 <student.andrey.2022@gmail.com>
2023-08-19 11:43:38 +0300

    added migration

--- news_site/news/models.py
+++ news_site/news/models.py
@@ -8,6 +8,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обнавлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True) # upload_to='photos/%Y/%m/%d' django cоздаст папку и будет хранить изображния
     is_publishished = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True) # связь между моделями
 
     def __str__(self):
         return self.title
