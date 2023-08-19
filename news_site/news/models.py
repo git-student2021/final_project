54001827a95db0213168fe5105e8cdf8ec90d271
git-student2021 <student.andrey.2022@gmail.com>
2023-08-19 09:08:39 +0300

    add str in model

--- news_site/news/models.py
+++ news_site/news/models.py
@@ -8,3 +8,6 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d') # upload_to='photos/%Y/%m/%d' django cоздаст папку и будет хранить изображния
     is_publishished = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
