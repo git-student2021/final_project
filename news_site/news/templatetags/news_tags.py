d64eb56e0d192cbb8cf96b6773ca242156068e93
git-student2021 <student.andrey.2022@gmail.com>
2023-08-20 10:25:25 +0300

    added new template list_categories

--- news_site/news/templatetags/news_tags.py
+++ news_site/news/templatetags/news_tags.py
@@ -4,6 +4,6 @@ from news.models import Category
 
 register = template.Library()
 
-@register.simple_tag()
+@register.simple_tag(name='get_list_categories')
 def get_categories():
     return Category.objects.all()
