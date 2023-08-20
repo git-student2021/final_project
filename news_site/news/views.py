6926ccb9c07d744fa7a1fa09a3fd5fa378356168
git-student2021 <student.andrey.2022@gmail.com>
2023-08-20 11:04:50 +0300

    added new template view_news

--- news_site/news/views.py
+++ news_site/news/views.py
@@ -15,3 +15,8 @@ def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news, 'category': category})
+
+def view_news(request, news_id):
+    """Подключаем кнопку Read more"""
+    news_item = News.objects.get(pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
