0986181c922915f7e7be4e9168c30fb34b6f586f
git-student2021 <student.andrey.2022@gmail.com>
2023-08-20 11:09:53 +0300

    updated models, category.html, index.html

--- news_site/news/models.py
+++ news_site/news/models.py
@@ -11,6 +11,9 @@ class News(models.Model):
     is_publishished = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория') # связь между моделями
 
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={"news_id": self.pk})
+
     def __str__(self):
         return self.title
 
