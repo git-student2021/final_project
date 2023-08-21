e4e0b9aad527e7e4fbc9c6f9facc555fc5c9ceea
git-student2021 <student.andrey.2022@gmail.com>
2023-08-21 11:21:18 +0300

    added form associated with the model

--- news_site/news/forms.py
+++ news_site/news/forms.py
@@ -23,3 +23,4 @@ class NewsForm(forms.ModelForm):
             'content': forms.Textarea(attrs={"class": "form-control", "rows": 5}),
             'category': forms.Select(attrs={"class": "form-control"}),
         }
+
